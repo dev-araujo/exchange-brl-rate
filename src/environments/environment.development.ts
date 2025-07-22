a1aa37f2bab967debef529a208fc3013bc3b10bd
araujo <theriancirculo@protonmail.com>
2025-07-22 16:29:07 -0300

    feat: Implement base app

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  API_KEY: 'RVZG0GHEV2KORLNA',
+  API_URL: 'https://api-brl-exchange.actionlabs.com.br/api/1.0/open',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  API_KEY: 'RVZG0GHEV2KORLNA',
+  API_URL:'https://api-brl-exchange.actionlabs.com.br/api/1.0/open'
+};
